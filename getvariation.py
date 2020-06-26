b9100d9916fad7d2680e9a3a0089c1eea2a6c157
Emmanuel Chamilakis <emmanuel.chamilakis@bloxs.com>
2020-06-26 18:54:10 +0200

    Added regex headers

--- getvariation.py
+++ getvariation.py
@@ -8,9 +8,25 @@ letters = list(map(chr, range(97, 118)))
 bold_list = list()
 filename = '../franciscanauthors_data/heads.txt'
 
+
+def get_bold(tag):
+    while True:
+        if tag:
+            test = tag.find('b')
+        else:
+            return None
+        if test is None or test == -1:
+            if tag.nextSibling is None:
+                return None
+            else:
+                tag = tag.nextSibling.nextSibling
+        elif test.name == 'b':
+            return test.text.strip()
+
+
 for let in letters:
+    #let = 'm'
     with open("../franciscanauthors_data/franaut"+let+".htm", "r") as f:
-    #with open("../franciscanauthors_data/"+any, "r") as f:
         contents = f.read()
 
         soup = BeautifulSoup(contents, 'html')
@@ -27,28 +43,18 @@ for let in letters:
         p = url.parent
         p = p.parent
         lemmas.append(p)
-
-
-def get_bold(tag):
-    while True:
-        test = tag.find('b')
-        if test is None or test == -1:
-            if tag.nextSibling is None:
-                return None
-            else:
-                tag = tag.nextSibling.nextSibling
-        elif test.name == 'b':
-            return test.text.strip()
+    if let == 'm':
+        lemmas.pop(0)
 
     for lemma in lemmas:
         tag = lemma.find('p')
-        tag = tag.nextSibling.nextSibling
+        if tag and tag.nextSibling:
+            tag = tag.nextSibling.nextSibling
+        else:
+            continue
         value = get_bold(tag)
         if value is not None and not value in bold_list:
             bold_list.append(value)
-
-
-
 
 with open(filename, mode="w") as outfile:  # also, tried mode="rb"
     for s in bold_list:
